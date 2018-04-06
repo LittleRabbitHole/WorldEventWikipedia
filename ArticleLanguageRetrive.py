83416592678039a39a486968d9683bef14646243
Angrrr_ <li.ang198744@gmail.com>
2018-04-06 10:55:56 -0400

    initial commit

--- ArticleLanguageRetrive.py
+++ ArticleLanguageRetrive.py
@@ -20,63 +20,54 @@ import itertools
 
 
 
-API = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=links&plnamespace=0&pllimit=500&titles="
-pageTitle = "Portal:Current_events/March_2017"
+#API = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=links&plnamespace=0&pllimit=500&titles="
+#pageTitle = "Portal:Current_events/March_2017"
 
+if __name__ == "__main__":
+    f = open("all133articles.txt")
+    articles = f.readlines()
+    f.close()
 
-
-
-os.chdir("/Users/Ang/OneDrive/Documents/Pitt_PhD/ResearchProjects/Wiki_Event/data")
-
-f = open("all133articles.txt")
-articles = f.readlines()
-f.close()
-
-line = articles[0].strip()
-splitline = line.split("\t")
-title = splitline[0]
-
-
-f_ch = open("articles_ch.csv", "w", encoding="UTF-8")
-f_span = open("articles_span.csv", "w", encoding="UTF-8")
-
-csv_ch_f = csv.writer(f_ch)
-csv_span_f = csv.writer(f_span)
-#write first row
-csv_ch_f.writerow(['title', 'lang-ch', 'lang-span'])
-csv_span_f.writerow(['title', 'lang-span'])
-
-n=0
-for line in articles:
-    n+=1
-    line = line.strip()
-    splitline = line.split("\t")
-    title = splitline[0]
-    event = splitline[1]
-    url = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=langlinks&llprop=langname&lllimit=500&titles=" + title
-    response=urllib.request.urlopen(url)
-    str_response=response.read().decode('utf-8')
-    responsedata = json.loads(str_response)
-    page_id = list(responsedata["query"]["pages"].keys())[0]
-    try:
-        lang_data_lst=responsedata['query']['pages'][page_id]['langlinks']
-        lang_list = list()
-        for item in lang_data_lst:
-            lang_list.append(item['langname'])
-        
-        if "Chinese" in lang_list: 
-            csv_ch_f.writerow([title, event, "chinese", ""])
-        else:
-            csv_ch_f.writerow([title, event,  "no chinese"])
+    f_ch = open("articles_ch.csv", "w", encoding="UTF-8")
+    f_span = open("articles_span.csv", "w", encoding="UTF-8")
+    
+    csv_ch_f = csv.writer(f_ch)
+    csv_span_f = csv.writer(f_span)
+    #write first row
+    csv_ch_f.writerow(['title', 'lang-ch', 'lang-span'])
+    csv_span_f.writerow(['title', 'lang-span'])
+    
+    n=0
+    for line in articles:
+        n+=1
+        line = line.strip()
+        splitline = line.split("\t")
+        title = splitline[0]
+        event = splitline[1]
+        url = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=langlinks&llprop=langname&lllimit=500&titles=" + title
+        response=urllib.request.urlopen(url)
+        str_response=response.read().decode('utf-8')
+        responsedata = json.loads(str_response)
+        page_id = list(responsedata["query"]["pages"].keys())[0]
+        try:
+            lang_data_lst=responsedata['query']['pages'][page_id]['langlinks']
+            lang_list = list()
+            for item in lang_data_lst:
+                lang_list.append(item['langname'])
             
-        if "Spanish" in lang_list: 
-            csv_span_f.writerow([title, event, "spanish", ""])
-        else:
-            csv_span_f.writerow([title, event, "no spanish"])
-            
-    except KeyError:  
-        csv_ch_f.writerow([title, event, "no other lang"])
-        csv_span_f.writerow([title, event, "no other lang"])
-
-f_ch.close()
-f_span.close()
+            if "Chinese" in lang_list: 
+                csv_ch_f.writerow([title, event, "chinese", ""])
+            else:
+                csv_ch_f.writerow([title, event,  "no chinese"])
+                
+            if "Spanish" in lang_list: 
+                csv_span_f.writerow([title, event, "spanish", ""])
+            else:
+                csv_span_f.writerow([title, event, "no spanish"])
+                
+        except KeyError:  
+            csv_ch_f.writerow([title, event, "no other lang"])
+            csv_span_f.writerow([title, event, "no other lang"])
+    
+    f_ch.close()
+    f_span.close()
