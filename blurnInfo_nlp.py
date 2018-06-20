99e78134d7b9bc3cd6a3f5b9f1bcb3ddb504b225
Angrrr_ <li.ang198744@gmail.com>
2018-06-19 23:37:02 -0400

    update

--- blurnInfo_nlp.py
+++ blurnInfo_nlp.py
@@ -19,6 +19,9 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.corpus import wordnet
 from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 
 wordnet_lemmatizer = WordNetLemmatizer()
 
@@ -37,7 +40,7 @@ def get_wordnet_pos(treebank_tag):
         return wordnet.NOUN
 
 
-def clean_blurb(blurb_row):
+def clean_blurb(c):
     #define patterns
     URL_PATTERN=re.compile(r'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?\xab\xbb\u201c\u201d\u2018\u2019]))')
     HASHTAG_PATTERN = re.compile(r'#\w*')
@@ -47,7 +50,7 @@ def clean_blurb(blurb_row):
     NUMBERS_PATTERN = re.compile(r"(^|\s)(\-?\d+(?:\.\d)*|\d+)")
 
     #clean url, emoji, mention, smiley
-    blurb = blurb_row['all_blurbs']
+    blurb = c['all_blurbs']
     clean_Url = re.sub(URL_PATTERN, '', blurb)
     clean_UrlRev = re.sub(RESERVED_WORDS_PATTERN, '', clean_Url)
     clean_final = re.sub(NUMBERS_PATTERN, '', clean_UrlRev)
@@ -61,11 +64,12 @@ def clean_blurb(blurb_row):
         #final remove leading/trailing puctuation each word
         word_lst1 = [s.strip("`~()?:!.,;'""&*<=+ >#|-/{}%$^@[]") for s in clean_lst]
         #remove non-letter in middle
-        word_lst = [re.sub(r'[^a-zA-Z0-9]+', '', s) for s in word_lst1]
+        word_lst = [re.sub(r'[^a-zA-Z]+', ' ', s) for s in word_lst1]
         #remove stop words
         filtered_words = [word for word in word_lst if word not in stopwords.words('english')]
         #remove all non-letters
         filtered_stopwords = [re.sub(r'^[^a-zA-Z]+$', '', s) for s in filtered_words]
+        filtered_stopwords = list(filter(None, filtered_stopwords))
         #lemm      
         tagged = nltk.pos_tag(filtered_stopwords)
         lemed_lst = [wordnet_lemmatizer.lemmatize(x[0], get_wordnet_pos(x[1])) for x in tagged]
@@ -88,4 +92,14 @@ blurb_data = pd.read_table("/Users/Ang/GoogleDrive/GoogleDrive_Pitt_PhD/UPitt_Ph
 blurb_data.columns.values
 blurb_data = blurb_data.fillna(" ")
 blurb_data['all_blurbs'] = blurb_data["blurb"] + " " + blurb_data["altblurb"] + blurb_data["altblurb2"] + blurb_data["altblurb3"]+ blurb_data["altblurb4"]
-blurb_data['clean_blurbs'] = blurb_data.apply(clean_blurb, )
+blurb_data['clean_blurbs'] = blurb_data.apply(clean_blurb, axis=1)
+
+# Initialize a CountVectorizer object: count_vectorizer
+count_vec = CountVectorizer(stop_words="english", analyzer='word', 
+                            ngram_range=(1, 1), max_df=1.0, min_df=1, max_features=None)
+
+# Transforms the data into a bag of words
+# https://medium.com/mlreview/topic-modeling-with-scikit-learn-e80d33668730
+# https://www.kaggle.com/adamschroeder/countvectorizer-tfidfvectorizer-predict-comments#
+count_train = count_vec.fit(txt)
+bag_of_words = count_vec.transform(txt)
