2c93ba74172656746f0127dead9896ad1a851dab
Angrrr_ <li.ang198744@gmail.com>
2018-06-20 21:44:02 -0400

    topic model update

--- blurnInfo_nlp.py
+++ blurnInfo_nlp.py
@@ -21,6 +21,7 @@ from nltk.corpus import wordnet
 from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import NMF, LatentDirichletAllocation
 
 
 wordnet_lemmatizer = WordNetLemmatizer()
@@ -43,10 +44,10 @@ def get_wordnet_pos(treebank_tag):
 def clean_blurb(c):
     #define patterns
     URL_PATTERN=re.compile(r'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?\xab\xbb\u201c\u201d\u2018\u2019]))')
-    HASHTAG_PATTERN = re.compile(r'#\w*')
-    MENTION_PATTERN = re.compile(r'@\w*')
+    #HASHTAG_PATTERN = re.compile(r'#\w*')
+    #MENTION_PATTERN = re.compile(r'@\w*')
     RESERVED_WORDS_PATTERN = re.compile(r'^(RT|FAV)')
-    SMILEYS_PATTERN = re.compile(r"(?:X|:|;|=)(?:-)?(?:\)|\(|O|D|P|S){1,}", re.IGNORECASE)
+    #SMILEYS_PATTERN = re.compile(r"(?:X|:|;|=)(?:-)?(?:\)|\(|O|D|P|S){1,}", re.IGNORECASE)
     NUMBERS_PATTERN = re.compile(r"(^|\s)(\-?\d+(?:\.\d)*|\d+)")
 
     #clean url, emoji, mention, smiley
@@ -87,19 +88,45 @@ def clean_blurb(c):
     return cleanedtext
 
 
-blurb_data = pd.read_table("/Users/Ang/GoogleDrive/GoogleDrive_Pitt_PhD/UPitt_PhD_O/Research/WorldEvents&Wikipedia/data/blurb_itn.csv", 
-                         sep=',', error_bad_lines = False)
-blurb_data.columns.values
-blurb_data = blurb_data.fillna(" ")
-blurb_data['all_blurbs'] = blurb_data["blurb"] + " " + blurb_data["altblurb"] + blurb_data["altblurb2"] + blurb_data["altblurb3"]+ blurb_data["altblurb4"]
-blurb_data['clean_blurbs'] = blurb_data.apply(clean_blurb, axis=1)
+def display_topics(model, feature_names, no_top_words):
+    for topic_idx, topic in enumerate(model.components_):
+        print ("Topic {}:".format(topic_idx))
+        print (" ".join([feature_names[i] for i in topic.argsort()[:-no_top_words - 1:-1]]))
 
-# Initialize a CountVectorizer object: count_vectorizer
-count_vec = CountVectorizer(stop_words="english", analyzer='word', 
-                            ngram_range=(1, 1), max_df=1.0, min_df=1, max_features=None)
 
-# Transforms the data into a bag of words
-# https://medium.com/mlreview/topic-modeling-with-scikit-learn-e80d33668730
-# https://www.kaggle.com/adamschroeder/countvectorizer-tfidfvectorizer-predict-comments#
-count_train = count_vec.fit(txt)
-bag_of_words = count_vec.transform(txt)
+if __name__ == "__main__":
+
+    blurb_data = pd.read_table("/Users/Ang/GoogleDrive/GoogleDrive_Pitt_PhD/UPitt_PhD_O/Research/WorldEvents&Wikipedia/data/blurb_itn.csv", 
+                             sep=',', error_bad_lines = False)
+    blurb_data.columns.values
+    blurb_data = blurb_data.fillna(" ")
+    blurb_data['all_blurbs'] = blurb_data["blurb"] + " " + blurb_data["altblurb"] + blurb_data["altblurb2"] + blurb_data["altblurb3"]+ blurb_data["altblurb4"]
+    blurb_data['clean_blurbs'] = blurb_data.apply(clean_blurb, axis=1)
+    blurb_data_clean = blurb_data['clean_blurbs'].tolist()
+    
+    # Initialize a CountVectorizer object: count_vectorizer
+    # https://medium.com/mlreview/topic-modeling-with-scikit-learn-e80d33668730
+    no_features = 1000
+    no_topics = 10
+    no_top_words = 10
+    
+    # NMF is able to use tf-idf
+    tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=2, max_features=no_features, stop_words='english')
+    tfidf = tfidf_vectorizer.fit_transform(blurb_data_clean)
+    tfidf_feature_names = tfidf_vectorizer.get_feature_names()
+
+    # Run NMF
+    nmf = NMF(n_components=no_topics, random_state=1, alpha=.1, l1_ratio=.5, init='nndsvd').fit(tfidf)
+    display_topics(nmf, tfidf_feature_names, no_top_words)
+    
+    # LDA can only use raw term counts for LDA because it is a probabilistic graphical model
+    tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2, max_features=no_features, stop_words='english')
+    tf = tf_vectorizer.fit_transform(blurb_data_clean)
+    tf_feature_names = tf_vectorizer.get_feature_names()
+
+    # Run LDA
+    lda = LatentDirichletAllocation(n_topics=no_topics, max_iter=5, learning_method='online', learning_offset=50.,random_state=0).fit(tf)
+    
+    display_topics(lda, tf_feature_names, no_top_words)
+
+    
