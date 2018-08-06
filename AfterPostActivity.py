e79a05e6465b089eb7de00580d15cf1461d6d1c7
Keyang Zheng <albuszheng@hotmail.com>
2018-08-05 20:36:18 -0400

    purpose a method to get the # of references and unique references

--- AfterPostActivity.py
+++ AfterPostActivity.py
@@ -10,15 +10,19 @@
 #  /w/api.php?action=compare&fromrev=[REV DIFF BASE ID]&torev=[REV DIFF TARGET ID]
 #             &prop=diff%7Ccomment%7Cuser%7Cparsedcomment&format=json&formatversion=2
 #
-# - the 'diffsize' in Wiki Compare API is the size different between 2 revision's HTML format
+# - The 'diffsize' in Wiki Compare API is the size different between 2 revision's HTML format
 #   not the in actural wiki text size, which should be calculated based on the size of every
 #   revision.
 #
-# - the 'parsedcomment' in Wiki Revision API is the raw HTML version of the comment showed in
+# - The 'parsedcomment' in Wiki Revision API is the raw HTML version of the comment showed in
 #   the article's revision page, which has a special character contain information like "section
 #   edit" (and the section title), "automatic edit summary". These information is not available
 #   in Revision API to be accessed, but seems useful enough to be included. The extraction should
 #   be a relatively easy job
+#
+# - By counting the number of reference items in article's Reference section, we should be able to 
+#   get the # of unique references. For the # of references, we need to count the actual # of 
+#   reference marks in the text (or the <ref> tags in wiki text)
 ##
 
 import csv
