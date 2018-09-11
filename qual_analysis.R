b3257b5e56035f71a9fc64cd8f6e868fd5bb0747
Angrrr_ <li.ang198744@gmail.com>
2018-09-11 00:05:57 -0400

    commit

--- qual_analysis.R
+++ qual_analysis.R
@@ -1,5 +1,27 @@
+qual_raw = read.csv("/Users/ang/GoogleDrive/GoogleDrive_Pitt_PhD/UPitt_PhD_O/Research/WorldEvents&Wikipedia/data/Ang/rev_candidate_full_section.csv")
+qual = qual_raw[c("external_links", "references", "size", "wiki_links","section_breadth", "section_depth", "section_num")]
+pca_fit = prcomp(qual, center = T, scale = T)
+#pca_fit$rotation
+sum_pca_fit = summary(pca_fit)
+pcs = data.frame(sum_pca_fit$x)
+pca_var_df = cbind.data.frame(1:length(as.numeric(sum_pca_fit$importance[2,])),
+                              as.numeric(sum_pca_fit$importance[2,]), 
+                              as.numeric(sum_pca_fit$importance[3,]))
+
+names(pca_var_df) = c("PC","Proportion Explained", "Cumulative Explained")
+pca_var_df
+o_qual = pcs$PC1
+summary(o_qual)
+qual_raw$qual_score = o_qual
+
+write.csv(qual_raw, file = "/Users/ang/GoogleDrive/GoogleDrive_Pitt_PhD/UPitt_PhD_O/Research/WorldEvents&Wikipedia/data/Ang/quality_data_score.csv", row.names=FALSE)
+
+
+library(ggplot2)
 qual_data = read.csv("/Users/angli/ANG/GoogleDrive/GoogleDrive_Pitt_PhD/UPitt_PhD_O/Research/WorldEvents&Wikipedia/data/Ang/quality_data.csv")
+qual_data = read.csv("/Users/ang/GoogleDrive/GoogleDrive_Pitt_PhD/UPitt_PhD_O/Research/WorldEvents&Wikipedia/data/Ang/quality_data.csv")
 colnames(qual_data)
+
 
 summary(as.factor(qual_data$topic_simple))
 #-1   0   3   6 
@@ -25,17 +47,63 @@ qual_data$loc_Spanish = 0
 qual_data$loc_Spanish[which(qual_data$location=="Spanish")] <- 1
 
 #size over location
+hist(qual_data$o3_qualscore)
 
-summary(lm(formula = log(o1_size+0.01) ~ loc_Chinese + loc_English + loc_EnglishPlus + loc_Spanish, 
+summary(lm(formula = qual_data$o1_qualscore ~ loc_Chinese + loc_English + loc_EnglishPlus + loc_Spanish, 
            data = qual_data))
 
-summary(lm(formula = log(o2_size+0.01) ~ log(o1_size+0.01) + loc_Chinese + loc_English + loc_EnglishPlus + loc_Spanish, 
+summary(lm(formula = qual_data$o2_qualscore ~ o1_qualscore + loc_Chinese + loc_English + loc_EnglishPlus + loc_Spanish, 
            data = qual_data))
 
-summary(lm(formula = log(o3_size+0.01) ~ log(o2_size+0.01) + loc_Chinese + loc_English + loc_EnglishPlus + loc_Spanish, 
+summary(lm(formula = o3_qualscore ~ o2_qualscore + loc_Chinese + loc_English + loc_EnglishPlus + loc_Spanish, 
            data = qual_data))
 
-summary(lm(formula = log(o4_size+0.01) ~ log(o3_size+0.01) + loc_Chinese + loc_English + loc_EnglishPlus + loc_Spanish, 
+summary(lm(formula = o4_qualscore ~ o3_qualscore + loc_Chinese + loc_English + loc_EnglishPlus + loc_Spanish, 
+           data = qual_data))
+
+
+
+summary(lm(formula = o1_qualscore ~ topic_mandisaster + topic_naturaldisaster + topic_science, 
+           data = qual_data))
+
+summary(lm(formula = o2_qualscore ~ o1_qualscore + topic_mandisaster + topic_naturaldisaster + topic_science, 
+           data = qual_data))
+
+summary(lm(formula = o3_qualscore ~ o2_qualscore + topic_mandisaster + topic_naturaldisaster + topic_science, 
+           data = qual_data))
+
+summary(lm(formula = o4_qualscore ~ o3_qualscore + topic_mandisaster + topic_naturaldisaster + topic_science, 
+           data = qual_data))
+
+
+summary(lm(formula = o1_qualscore ~ as.factor(language), 
+           data = qual_data))
+
+summary(lm(formula = o2_qualscore ~ o1_qualscore + as.factor(language), 
+           data = qual_data))
+
+summary(lm(formula = o3_qualscore ~ o2_qualscore + as.factor(language), 
+           data = qual_data))
+
+summary(lm(formula = o4_qualscore ~ o3_qualscore + as.factor(language), 
+           data = qual_data))
+
+
+#across community over localtion
+summary(lm(formula = o1_qualscore ~ as.factor(language)*as.factor(location), 
+           data = qual_data))
+
+summary(lm(formula = o2_qualscore ~ o1_qualscore + as.factor(language)*as.factor(location), 
+           data = qual_data))
+
+summary(lm(formula = o4_qualscore ~ o3_qualscore + as.factor(language)*as.factor(location), 
+           data = qual_data))
+
+## across topics
+summary(lm(formula = o1_qualscore ~ as.factor(language)*as.factor(topic_simple), 
+           data = qual_data))
+
+summary(lm(formula = o4_qualscore ~ o3_qualscore+as.factor(language)*as.factor(topic_simple), 
            data = qual_data))
 
 
@@ -53,7 +121,6 @@ summary(lm(formula = log(o3_size+0.01) ~ log(o2_size+0.01) + topic_mandisaster +
 
 summary(lm(formula = log(o4_size+0.01) ~ log(o3_size+0.01) + topic_mandisaster + topic_naturaldisaster + topic_science, 
            data = qual_data))
-
 
 # size over communities
 
