91ac65d43ca9eae1b33418f4eb7c1d2b834c7ab1
LittleRabbitHole <li.ang198744@gmail.com>
2018-11-29 00:38:34 -0500

    Update LangLinks.py

--- LangLinks.py
+++ LangLinks.py
@@ -3,6 +3,7 @@
 """
 Created on Wed Nov 28 17:02:50 2018
 Language link collection for a data frame with article title in English Wikipedia
+<-> https://zh.wikipedia.org/?curid=1543200
 @author: angli
 """
 
@@ -16,6 +17,9 @@ import csv
 import os
 import requests
 import sys
+import pickle
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 
 def returnJsonCheck(response) -> dict:
@@ -26,27 +30,109 @@ def returnJsonCheck(response) -> dict:
         print(response)
         print(response.text)
         sys.exit("json error")
-        
+       
 
-def getLanglinks(title):
-    api = "https://en.wikipedia.org/w/api.php?action=query&titles={}&prop=langlinks&lllimit=500&llprop=langname|url&format=json".format(title)
-    response =requests.get(api)
+
+def getPageId(lang, title):
+    api = "https://{}.wikipedia.org/w/api.php?action=query&format=json&titles={}".format(lang, title)
+
+    #set session
+    session = requests.Session()
+    retry = Retry(connect=3, backoff_factor=0.5)
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+
+    #use session to retrive data
+    #response =requests.get(api)
+    response =session.get(api)
     responsedata = returnJsonCheck(response)
     page_id = list(responsedata["query"]["pages"].keys())[0]
-    alllanglst=responsedata['query']['pages'][page_id]['langlinks']
-    for langitem in alllanglst:
-        if langitem['lang'] in ['ar', 'zh', 'es']:
-            title = langitem['*']
-            url = langitem['url']
-            lang = langitem['lang']
-        
+    return page_id
+
+
+def getLanglinks(title):
+    api = "https://en.wikipedia.org/w/api.php?action=query&format=json&titles={}&prop=langlinks&lllimit=500&llprop=langname|url".format(title)
+    
+    #set session
+    session = requests.Session()
+    retry = Retry(connect=3, backoff_factor=0.5)
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    
+    #response =requests.get(api)
+    response =session.get(api)
+    responsedata = returnJsonCheck(response)
+    page_id = list(responsedata["query"]["pages"].keys())[0]
+    alllanglst=responsedata['query']['pages'][page_id].get('langlinks')
+    
+    article_lang_links = {'ar':None, 'zh':None, 'es':None}
+    if alllanglst is not None:
+        for langitem in alllanglst:
+            if langitem['lang'] == 'ar':#['ar', 'zh', 'es']:
+                ar_title = langitem['*']
+                ar_url = langitem['url']
+                #lang = langitem['lang']
+                ar_pageid = getPageId('ar', ar_title)
+                #print (lang)
+                article_lang_links['ar'] = [ar_title, ar_pageid, ar_url]
+            elif langitem['lang'] == 'zh':
+                zh_title = langitem['*']
+                zh_url = langitem['url']
+                #lang = langitem['lang']
+                zh_pageid = getPageId('zh', zh_title)
+                #print (lang)
+                article_lang_links['zh'] = [zh_title, zh_pageid, zh_url]
+            elif langitem['lang'] == 'es':
+                es_title = langitem['*']
+                es_url = langitem['url']
+                #lang = langitem['lang']
+                es_pageid = getPageId('es', es_title)
+                #print (lang)
+                article_lang_links['es'] = [es_title, es_pageid, es_url]
+    
+    return (page_id, article_lang_links)    
     
 
 
 
-data = pd.read_table("/Users/angli/ANG/GoogleDrive/GoogleDrive_Pitt_PhD/UPitt_PhD_O/Research/WorldEventsWikipedia/data/Ang/revise/posted_itn_filter.csv", 
-                             sep=',', error_bad_lines = False)
 
-for index, row in data.iterrows():
-    title = row['article']
+if __name__ == "__main__":
+    data = pd.read_table("/Users/angli/ANG/GoogleDrive/GoogleDrive_Pitt_PhD/UPitt_PhD_O/Research/WorldEventsWikipedia/data/Ang/revise/posted_itn_filter_death.csv", 
+                                 sep=',', error_bad_lines = False)
+    data = pd.read_table("/Users/jiajunluo/GoogleDrive/GoogleDrive_Pitt_PhD/UPitt_PhD_O/Research/WorldEventsWikipedia/data/Ang/revise/posted_itn_filter_death.csv", 
+                                 sep=',', error_bad_lines = False)
+    
+    #collect the langlinks for all the articles
+    all_article_langlinks = []
+    i=0
+    for index, row in data.iterrows():
+        i+=1
+        postid = str(int(row['postid']))
+        post_year = str(int(row['year']))
+        post_date = row['time']
+        #article
+        title = row['article']
+        pageid, article_lang_links = getLanglinks(title)
+        article_lang_links['en'] = [title, pageid]
+        all_article_langlinks.append([postid, post_year, post_date, title, pageid, article_lang_links])
+        if i%10==0: print (title)
+    
+    f = open('/Users/jiajunluo/GoogleDrive/GoogleDrive_Pitt_PhD/UPitt_PhD_O/Research/WorldEventsWikipedia/data/Ang/revise/all_article_langlinks.pkl', 'wb')   # Pickle file is newly created where foo1.py is
+    pickle.dump(all_article_langlinks, f)          # dump data to f
+    f.close() 
+
+    f = open('/Users/jiajunluo/GoogleDrive/GoogleDrive_Pitt_PhD/UPitt_PhD_O/Research/WorldEventsWikipedia/data/Ang/revise/all_article_langlinks.pkl', 'rb')   # 'r' for reading; can be omitted
+    mydict = pickle.load(f)         # load file content as mydict
+    f.close()
+
+    
+    
+    
+    
+    
+    
+    
+    
     
